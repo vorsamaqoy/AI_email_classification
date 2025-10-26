f1172bac13fbad6fd64d37c17636ee01f4d3b283
Vincenzo Vigna <166010612+vorsamaqoy@users.noreply.github.com>
2025-10-26 11:46:39 +0100

    Update author name in __init__.py

--- __init__.py
+++ __init__.py
@@ -7,11 +7,12 @@ from .email_classifier import EmailClassifier, ConfigurableEmailClassifier
 from .config.models import ClassifierConfig, create_sample_config
 
 __version__ = "3.0.0"
-__author__ = "Your Name"
+__author__ = "Vincenzo Vigna"
 
 __all__ = [
     'EmailClassifier',
     'ConfigurableEmailClassifier', 
     'ClassifierConfig',
     'create_sample_config'
+
 ]
