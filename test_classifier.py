808aff50951300a53ee31f1c6dfc6e3b0c299951
Vincenzo Vigna <166010612+vorsamaqoy@users.noreply.github.com>
2025-10-26 12:04:28 +0100

    Update test_classifier.py

--- test_classifier.py
+++ test_classifier.py
@@ -1,4 +1,4 @@
-from email_classifier import run_configurable_validation
+from email_classifier import run_validation_suite
 
 if __name__ == "__main__":
-    classifier = run_configurable_validation()
+    classifier = run_validation_suite()
